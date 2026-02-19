f443fbe8179bc5c518840d91879b74624af4fc4f
tarkoman4ik <tarkoman4ik@vk.com>
2026-02-19 07:55:41 +0500

    initial commit

---
+++ src/main/java/com/example/bankcards/EffectiveMobileTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveMobileTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankcards/EffectiveMobileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
